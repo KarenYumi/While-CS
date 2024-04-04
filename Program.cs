81e662af17c536fd8a6bf8dec3bbee5f9a3be2b0
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-04-03 22:28:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a senha desta conta: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while(senha != 2002)
+            {
+                Console.WriteLine("Senha incorreta");
+                Console.WriteLine("Digite a senha desta conta: ");
+                 senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha correta!");
+
+
+        }
+    }
+}
